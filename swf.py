e6da65dce25a50f87d2cd858cf3bda2cc2cec135
lemoer <git@irrelefant.net>
2021-12-12 22:05:46 +0100

    Implement my_sph_harm()

--- swf.py
+++ swf.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm, colors
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from scipy.special import sph_harm
+from scipy.special import sph_harm, factorial, lpmv
 
 phi = np.linspace(0, 2*np.pi, 100)
 theta = np.linspace(0, np.pi, 100)
@@ -64,6 +64,44 @@ def vec_c2s(f_x, f_y, f_z, r, theta, phi):
 # plt.figure()
 # plt.plot(F_z)
 
+def my_sph_harm(l, m, theta, phi):
+    # This is a copy of sph_harm() with different argument order based
+    # on Kristenson, p. 95. It has been validated for some examples,
+    # that we get the same results as sph_harm().
+
+    # we leave out the (-1)**m here in c_lm, because it is already
+    # part of the legendre definition in lpmv(). 
+    c_lm = np.sqrt((2*l+1)/(4*np.pi)*factorial(l-m)/factorial(l+m))
+
+    exp_part = np.exp(1j*m*phi)
+    # NOTE: There is also lpmn(), which could be more efficient, if
+    # multiple l and m should be calculated... Optimize here in
+    # future, if you need better performance.
+    leg_part = lpmv(m, l, np.cos(theta))
+
+    return c_lm*exp_part*leg_part
+
+# Check my_sph_harm() as 1d fun...
+# l, m = 4, 2
+# # phi = np.pi/4
+# # theta = np.linspace(0, np.pi, 100)
+# phi = np.linspace(0, 2*np.pi, 100)
+# theta = np.pi/4
+
+# ref = sph_harm(m, l, phi, theta).imag
+# my = my_sph_harm(l, m, theta, phi).imag
+
+# plt.figure()
+# plt.plot(phi*180/np.pi, ref)
+# p = plt.plot(phi*180/np.pi, my, linestyle='--')
+
+def sph_harm_diff_phi(theta, phi):
+    # differentation of my_sph_harm() to phi (with conventions
+    # that phi is the azimuth.)
+    return 1j*m*my_sph_harm
+
+def sph_harm_diff_theta(theta, phi):
+    pass
 
 x, y, z = coord_s2c(1, theta, phi)
 #r, theta, phi = coord_c2s(x, y, z)
@@ -74,7 +112,8 @@ m, l = 0, 4
 
 # Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
 # Be cationous, sph_harm takes other args! 
-fcolors = sph_harm(m, l, phi, theta).real
+#fcolors = sph_harm(m, l, phi, theta).real
+fcolors = my_sph_harm(l, m, theta, phi).real
 fmax, fmin = fcolors.max(), fcolors.min()
 fcolors = (fcolors - fmin)/(fmax - fmin)
 
