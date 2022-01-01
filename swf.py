de47c25592317126ae25f543b81f001f806e2ad1
lemoer <git@irrelefant.net>
2022-01-01 20:04:38 +0100

    Implement vec_sph_harm()

--- swf.py
+++ swf.py
@@ -95,13 +95,77 @@ def my_sph_harm(l, m, theta, phi):
 # plt.plot(phi*180/np.pi, ref)
 # p = plt.plot(phi*180/np.pi, my, linestyle='--')
 
-def sph_harm_diff_phi(theta, phi):
-    # differentation of my_sph_harm() to phi (with conventions
+def sph_harm_diff_phi(l, m, theta, phi):
+    # differentation of my_sph_harm() w.r.t. phi (with conventions
     # that phi is the azimuth.)
-    return 1j*m*my_sph_harm
+    return 1j*m*my_sph_harm(l, m, theta, phi)
 
-def sph_harm_diff_theta(theta, phi):
-    pass
+def lpmv_diff_times_minus_sqrt_1_minus_x_squared(m, l, x):
+    # This function calculates the derivative of the legendre function lpmv()
+    # multiplied with a factor of -sqrt(1-x²), as in Kristenson 2014 p. 84
+    # (the last formula on the page, with an additional minus on both sides
+    # of the equation). This makes sense, because we actually want to derive 
+    # lpmv(m, l, np.cos(theta)) and the factor -sqrt(1-x²) exactly corresponds
+    # to the inner derivative of the np.cos(theta). So this function here 
+    # can be used directly to calculate the derivative. See test_lpmv_diff_stuff().
+    return -(1/2)*((l-m+1)*(l+m)*lpmv(m-1, l, x)-lpmv(m+1, l, x)) # grundmann
+
+def test_lpmv_diff_stuff():
+    plt.figure()
+    theta = np.linspace(0, np.pi, 50)
+    dtheta = theta[1] - theta[0]
+
+    # theta values in between the original values ("half-step")
+    theta_ = np.convolve(theta, [0.5, 0.5], 'valid')
+    
+    for l in range(3, 4):
+        m = 1
+        leg_part = lpmv(m, l, np.cos(theta))
+        leg_diff_analytic = lpmv_diff_times_minus_sqrt_1_minus_x_squared(m, l, np.cos(theta))
+        leg_diff_numeric = np.diff(leg_part)/dtheta
+
+        plt.plot(theta, leg_part)
+        # The following two graphs should be equal
+        plt.plot(theta, leg_diff_analytic)
+        plt.plot(theta_, leg_diff_numeric, ':')
+
+def sph_harm_diff_theta(l, m, theta, phi):
+    # we leave out the (-1)**m here in c_lm, because it is already
+    # part of the legendre definition in lpmv(). 
+    c_lm = np.sqrt((2*l+1)/(4*np.pi)*factorial(l-m)/factorial(l+m))
+
+    exp_part = np.exp(1j*m*phi)
+    # Inner derivation of the lpmv() argument:
+    # x = np.cos(theta)
+    # dx/dtheta = -sin(theta) = -sqrt(1-cos(theta)²) = -sqrt(1-x²)
+    leg_part = lpmv_diff_times_minus_sqrt_1_minus_x_squared(m, l, np.cos(theta))
+
+    return c_lm*exp_part*leg_part
+
+def vec_sph_harm(tau, l, m, theta, phi):
+    norm_fact = np.sqrt(l*(l+1))
+    if tau == 1 or tau == 2:
+        sin_theta = np.sin(theta)
+        idx_pole = np.where(theta == 0)[0]
+        sin_theta[idx_pole] = 1 # TODO: this is a hacky way of pole-zero
+                                # cancellation
+
+    if tau == 1:
+        f_theta = 1/(norm_fact*sin_theta)*sph_harm_diff_phi(l, m, theta, phi)
+        f_phi = -1/norm_fact*sph_harm_diff_theta(l, m, theta, phi)
+        f_r = np.array([0])
+    elif tau == 2:
+        f_theta = 1/norm_fact*sph_harm_diff_theta(l, m, theta, phi)
+        f_phi = 1/(norm_fact*sin_theta)*sph_harm_diff_phi(l, m, theta, phi)
+        f_r = np.array([0])
+    elif tau == 3:
+        f_theta = np.array([0])
+        f_phi = np.array([0])
+        f_r = my_sph_harm(l, m, theta, phi)
+    else:
+        raise Exception('NIY')
+
+    return f_r, f_theta, f_phi
 
 def fix_dim(A, axis):
     # Repeats the last column or row of a matrix once, so the dimension
@@ -118,13 +182,6 @@ def calc_dA(theta, phi):
     dtheta = fix_dim(np.diff(theta, axis=1), axis=1)
     dphi = fix_dim(np.diff(phi, axis=0), axis=0)
     return dtheta * dphi * np.sin(theta)
-
-
-x, y, z = coord_s2c(1, theta, phi)
-#r, theta, phi = coord_c2s(x, y, z)
-#x, y, z = coord_s2c(1, theta, phi)
-
-dA = calc_dA(theta, phi)
 
 def check_correlation_for_my_sph_harm(theta, phi, dA):
     m = 0
@@ -152,23 +209,71 @@ def check_correlation_for_my_sph_harm(theta, phi, dA):
 
     return C, f1, f2
 
+def test_correlation_for_vec_sph_harm(theta, phi, dA, tau1, tau2, m1, m2):
+    L = 10
+    C = np.zeros((L, L), dtype=complex)
+
+    # make arrays 1D, so we can use the dot() properly.
+    theta = np.ravel(theta)
+    phi = np.ravel(phi)
+    dA = np.ravel(dA)
+
+    # do the work
+    for l in range(1, L+1):
+        for l2 in range(1, L+1):
+            f1_r, f1_th, f1_ph = vec_sph_harm(tau1, l, m1, theta, phi)
+            f2_r, f2_th, f2_ph = vec_sph_harm(tau2, l2, m2, theta, phi)
+            
+            C[l-1, l2-1] = np.sum(f2_r.conj()*dA*f1_r) + np.sum(f2_th.conj()*dA*f1_th) + np.sum(f2_ph.conj()*dA*f1_ph)
+
+    plt.figure()
+    plt.imshow(np.abs(C))
+    plt.colorbar()
+    plt.title(f"$\\tau_1 = {tau1}, \\tau_2 = {tau2}, m_1 = {m1}, m_2 = {m2}$")
+    # plt.clim([0, 1])
+
+    return C
+
+def test_vec_sph_harm():
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 1, 1, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 1, 0, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 2, 2, 1, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 2, 2, 0, 0)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 2, 2, 0, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 2, 1, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 2, 1, 0)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 2, 0, 0)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 3, 1, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 1, 3, 0, 0)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 2, 3, 1, 1)
+    test_correlation_for_vec_sph_harm(theta, phi, dA, 2, 3, 0, 0)
 
 
-check_correlation_for_my_sph_harm(theta, phi, dA)
+x, y, z = coord_s2c(1, theta, phi)
+#r, theta, phi = coord_c2s(x, y, z)
+#x, y, z = coord_s2c(1, theta, phi)
 
-m, l = 0, 4
+dA = calc_dA(theta, phi)
 
-# Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
-# Be cationous, sph_harm takes other args! 
-#fcolors = sph_harm(m, l, phi, theta).real
-fcolors = my_sph_harm(l, m, theta, phi).real
-fmax, fmin = fcolors.max(), fcolors.min()
-fcolors = (fcolors - fmin)/(fmax - fmin)
+# test_lpmv_diff_stuff()
+# check_correlation_for_my_sph_harm(theta, phi, dA)
+test_vec_sph_harm()
 
-# Set the aspect ratio to 1 so our sphere looks spherical
-fig = plt.figure(figsize=plt.figaspect(1.))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z,  rstride=1, cstride=1, facecolors=cm.seismic(fcolors))
-# Turn off the axis planes
-ax.set_axis_off()
-plt.show()
+# m, l = 0, 4
+
+# # Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
+# # Be cationous, sph_harm takes other args! 
+# #fcolors = sph_harm(m, l, phi, theta).real
+# fcolors = my_sph_harm(l, m, theta, phi).real
+# fmax, fmin = fcolors.max(), fcolors.min()
+# fcolors = (fcolors - fmin)/(fmax - fmin)
+
+# # Set the aspect ratio to 1 so our sphere looks spherical
+# fig = plt.figure(figsize=plt.figaspect(1.))
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot_surface(x, y, z,  rstride=1, cstride=1, facecolors=cm.seismic(fcolors))
+# # Turn off the axis planes
+# ax.set_axis_off()
+# plt.show()
+
+print("end")
