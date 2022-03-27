b22365fb631f99acbbe2eb78005a6b69ef908d40
lemoer <git@irrelefant.net>
2022-03-27 14:35:40 +0200

    examples: FT of bessel: remove approx for integral of P

--- examples/test_bessel_fourier.py
+++ examples/test_bessel_fourier.py
@@ -12,7 +12,6 @@ from scipy.special import lpmv, spherical_jn, spherical_yn, eval_legendre
 
 l = 1
 
-MAGIC_UNKNOWN_NUMBER = 1.566 # TODO: Not sure, where this comes from. We should figure out...
 T_MAX = 2
 NT = 1000 # The higher this time is, the better the approximations of the P at the origin become. (Probably aliasing)
 
@@ -30,24 +29,13 @@ dt = t[1] - t[0]
 J = spherical_jn(l, x)
 J_by_x = spherical_jn(l, x)/x
 P = eval_legendre(l, t_col) * window_col
-# TODO: I am not completely sure, where the dt comes from in the next formula...
-P_conv = np.convolve(P.ravel(), -1j*(np.sign(t_col)).ravel()*dt/np.pi*MAGIC_UNKNOWN_NUMBER, mode="same")
-P_conv = P_conv[:, np.newaxis]*window_col # This additional windowing is in order 
-                                          # to cancel truncation errors due to convolution.
-
-# error in imag part is higher for this equation.
-P_int = -1j*np.cumsum(P)*dt
-P_int = P_int[:, np.newaxis]
-
 # This is the analytical solution of the integral of P
-P_ana = -1j*(eval_legendre(l+1, t_col) - eval_legendre(l-1, t_col))/(2*l+1)*window_col
+P_integral_ana = -1j*(eval_legendre(l+1, t_col) - eval_legendre(l-1, t_col))/(2*l+1)*window_col
 
 FT = np.matrix(np.exp(1j*t_row*x_col)) * dt # fourier transform
 
 J_integral_repr = 1/(2*(1j**l))*FT*P
-J_by_x_integral_repr = 1/(2*(1j**l))*FT*P_conv
-J_by_x_integral_repr2 = 1/(2*(1j**l))*FT*P_int
-J_by_x_integral_repr3 = 1/(2*(1j**l))*FT*P_ana
+J_by_x_integral_repr = 1/(2*(1j**l))*FT*P_integral_ana
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -61,17 +49,11 @@ ax1.plot(x, np.real(J_integral_repr), '--')
 ax1.plot(x, np.imag(J_integral_repr))
 
 ax2.plot(x, J_by_x)
-#ax2.plot(x, np.real(J_by_x_integral_repr), '--')
-#ax2.plot(x, np.imag(J_by_x_integral_repr))
-#ax2.plot(x, np.real(J_by_x_integral_repr2), ':')
-#ax2.plot(x, np.imag(J_by_x_integral_repr2))
-ax2.plot(x, np.real(J_by_x_integral_repr3), '--')
-ax2.plot(x, np.imag(J_by_x_integral_repr3))
+ax2.plot(x, np.real(J_by_x_integral_repr), '--')
+ax2.plot(x, np.imag(J_by_x_integral_repr))
 
 plt.show(block=True)
 
 plt.figure()
 plt.plot(t, P)
-plt.plot(t, np.imag(P_conv))
-plt.plot(t, np.imag(P_int), '--')
-plt.plot(t, np.imag(P_ana))
+plt.plot(t, np.imag(P_integral_ana))
